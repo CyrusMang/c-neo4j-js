f3af2bcd42b39bbcaf556a8153e73847c9456bea
Cyrus Mang <cyrusmang@httpeace.com>
2017-02-01 19:38:27 +0800

    [update]

---
+++ src/test/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import assert from 'assert'
+
+describe('database', () => {
+    describe('connect', () => {
+        it('should return a list of errors when value not match with schema', () => {
+            //
+        })
+    })
+})
